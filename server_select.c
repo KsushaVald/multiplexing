070a2e0340cae52ab3a03b703c35568a394cf02f
KsushaVald <ksushavald@yandex.ru>
2017-07-20 10:33:31 +0600

    finished

--- server_select.c
+++ server_select.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <sys/epoll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/time.h>
+#include <unistd.h>
 #include <time.h>
 #include <fcntl.h>
 #include <string.h>
+
 
 int main(){
         int fd_socket_tcp, fd_socket_udp, fd_client; int test;
         struct sockaddr_in my_addr_tcp, my_addr_udp, client_udp, client_tcp;
         socklen_t len_t=sizeof(struct sockaddr);
         socklen_t len_u=sizeof(struct sockaddr);
-        int fd_epoll; struct epoll_event  event[2], list;
+        fd_set set[2]; fd_set rset;
 	char time[256]; char buf[256];
         struct  tm *m_time; long int s_time;
 
@@ -29,42 +31,39 @@ int main(){
         fd_socket_tcp=socket(AF_INET, SOCK_STREAM,0);
         fd_socket_udp=socket(AF_INET,SOCK_DGRAM,0);
 
-	if(bind(fd_socket_tcp,(struct sockaddr*)&my_addr_tcp,sizeof(my_addr_tcp))==-1){
+        if(bind(fd_socket_tcp,(struct sockaddr*)&my_addr_tcp,sizeof(my_addr_tcp))==-1){
                 perror("bind");
         }
         if(bind(fd_socket_udp,(struct sockaddr*)&my_addr_udp,sizeof(my_addr_udp))==-1){
-                perror("bind");
+          perror("bind");
         }
         listen(fd_socket_tcp,1);
-	fd_epoll=epoll_create(2);
-	event[0].events=EPOLLIN;
-	event[0].data.fd=fd_socket_tcp;
-	event[1].events=EPOLLIN;
-	event[1].data.fd=fd_socket_udp;
 
-	epoll_ctl(fd_epoll,EPOLL_CTL_ADD,fd_socket_tcp,&event[0]);
-	epoll_ctl(fd_epoll,EPOLL_CTL_ADD,fd_socket_udp,&event[1]);
  	while(1){
-                test=epoll_wait(fd_epoll,&list,2,2000);
+		FD_ZERO(set);
+		FD_SET(fd_socket_tcp,&set[0]);
+		FD_SET(fd_socket_udp,&set[1]);
+                test=select(fd_socket_udp+1,set,NULL,NULL,NULL);
                 if(test==-1){
-                        perror("epol_wait");
+                        perror("select");
                         exit(-1);
                 }
                 else{
-                        if(test==!0){
+                        if(test!=0){
                                 s_time=times(NULL);
                                 m_time=localtime(&s_time);
                                 strcpy(time,asctime(m_time));
-                                if(list.data.fd==fd_socket_tcp){
-					fd_client=accept(fd_socket_tcp,(struct sockaddr*)&client_tcp,&len_t);
+                                if(FD_ISSET(fd_socket_tcp, &set[0])){
+                                        fd_client=accept(fd_socket_tcp,(struct sockaddr*)&client_tcp,&len_t);
                                         send(fd_client,time,sizeof(time),0);
                                 }
-                                if(list.data.fd==fd_socket_udp){
-					recvfrom(fd_socket_udp,&buf,sizeof(time),0,(struct sockaddr*)&client_udp,&len_u);
+                                if(FD_ISSET(fd_socket_udp,&set[1])){
+                                        recvfrom(fd_socket_udp,&buf,sizeof(time),0,(struct sockaddr*)&client_udp,&len_u);
                                         sendto(fd_socket_udp,time,sizeof(time), 0,(struct sockaddr*)&client_udp,len_u);
                                 }
                         }
                 }
         }
 }
+
 
